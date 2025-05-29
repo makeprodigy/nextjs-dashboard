5369fd18dfb200e5c4c320378013c7ab20e5fdc0
makeprodigy <payneparihar13@gmail.com>
2025-05-30 02:21:54 +0530

    chore: removed unused imports in layout to fix build errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import AuthNav from "@/components/AuthNav";
 import NextAuthSessionProvider from "@/components/SessionProvider";
 import Navbar from "@/components/Navbar";
 
